96a32aee09a69fe33205f1164adf5823d078ff2f
PH31M <ducminh03102005@gmail.com>
2026-05-10 15:54:17 +0700

    patch modelBooking

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -5,23 +5,22 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 class Booking(models.Model):
 
-    class Status(models.TextChoices):
+    class BookingStatus(models.TextChoices):
         PENDING   = 'pending'
         CONFIRMED = 'confirmed'
         CANCELLED = 'cancelled'
-        COMPLETED = 'completed'
+    
+    class PaymentStatus(models.TextChoices):
+        UNPAID = 'unpaid'
+        PAID = 'paid'
+        FAILED = 'failed'
+        REFUNDED = 'refunded'
 
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='bookings')
-    # service = models.ForeignKey('services.BaseService', on_delete=models.CASCADE, related_name='bookings')
-    # room_type = models.ForeignKey('services.RoomType', on_delete=models.SET_NULL, null=True, blank=True,related_name='bookings')
-    # seat_type = models.ForeignKey('services.SeatType', on_delete=models.SET_NULL, null=True, blank=True,related_name='bookings')
+    class PaymentMethod(models.TextChoices):
+        CASH = 'cash'
+        MOMO = 'momo'
+        VNPAY = 'vnpay'
 
-    # quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
-    # total_price = models.DecimalField(max_digits=12,decimal_places=2,validators=[MinValueValidator(0)])
-    # booking_status = models.CharField(max_length=20, choices = Status.choices, default=Status.PENDING)
-
-    # created_date = models.DateTimeField(auto_now_add=True)
-    # updated_date = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='bookings')
     service = models.ForeignKey('services.BaseService', on_delete=models.PROTECT, related_name='bookings')
@@ -29,10 +28,17 @@ class Booking(models.Model):
     seat_type = models.ForeignKey('services.SeatType', on_delete=models.SET_NULL, null=True, blank=True, related_name='bookings')
 
     quantity = models.PositiveIntegerField(default=1, validators = [MinValueValidator(1)])
+    total_price = models.DecimalField(max_digits=12, decimal_places=2, validators = [MinValueValidator(0)])
+    booking_status = models.CharField(max_length=20, choices = BookingStatus.choices, default=BookingStatus.PENDING)
+
+    payment_status = models.CharField(max_length=20, choices = PaymentStatus.choices, default=PaymentStatus.UNPAID)
+    payment_method = models.CharField(max_length=50, choices = PaymentMethod.choices, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_date']
 
     def __str__(self):
-        return f"Booking :{self.id}, {self.name} , {self.service.name}"
+        return f"Booking :{self.id}, {self.user.email} , {self.service.name}"
     
